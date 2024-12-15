c92d40d33f28bbf6355ca8f75353f7d27901de62
michalwachowicz <wachowiczmichal1@gmail.com>
2024-12-15 20:20:00 +0100

    Add forwardRef to the icon button

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable react/require-default-props */
+import React, { ForwardedRef } from "react";
 
 interface Props {
   label: string;
@@ -10,26 +11,33 @@ interface Props {
   children: React.ReactNode;
 }
 
-const IconButton = ({
-  label,
-  size = 9,
-  disabled = false,
-  onClick = () => {},
-  ariaControls = undefined,
-  ariaExpanded = undefined,
-  children,
-}: Props) => (
-  <button
-    aria-label={label}
-    className={`btn btn-icon w-${size} h-${size}`}
-    type="button"
-    onClick={onClick}
-    aria-controls={ariaControls}
-    aria-expanded={ariaExpanded}
-    disabled={disabled}
-  >
-    {children}
-  </button>
+const IconButton = React.forwardRef(
+  (props: Props, ref: ForwardedRef<HTMLButtonElement>) => {
+    const {
+      label,
+      size = 9,
+      disabled = false,
+      onClick = () => {},
+      ariaControls = undefined,
+      ariaExpanded = undefined,
+      children,
+    } = props;
+
+    return (
+      <button
+        ref={ref}
+        aria-label={label}
+        className={`btn btn-icon w-${size} h-${size}`}
+        type="button"
+        onClick={onClick}
+        aria-controls={ariaControls}
+        aria-expanded={ariaExpanded}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    );
+  },
 );
 
 export default IconButton;
