f2103b6ec3c7f351b2d715d0ca2398c527df531a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-12-23 12:47:11 +0100

    Customize getMovieDetails function

--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -21,9 +21,26 @@ const querySearch = async (query: string, page: number = 1) =>
     `/search/movie?query=${query.replace(" ", "%20")}&include_adult=false&language=en-US&page=${page}`,
   );
 
-const getMovieDetails = async (id: number) =>
-  requestData(
-    `/movie/${id}?append_to_response=credits%2Cimages%2Cvideos%2Csimilar&language=en-US`,
-  );
+const getMovieDetails = async (
+  id: number,
+  options?: {
+    credits?: boolean;
+    images?: boolean;
+    videos?: boolean;
+    similar?: boolean;
+  },
+) => {
+  let url = `/movie/${id}?language=en-US`;
+  const params: string[] = [];
+
+  if (options?.credits) params.push("credits");
+  if (options?.images) params.push("images");
+  if (options?.videos) params.push("videos");
+  if (options?.similar) params.push("similar");
+
+  if (params.length > 0) url += `?append_to_response=${params.join("%2C")}`;
+
+  return requestData(url);
+};
 
 export { querySearch, getMovieDetails };
