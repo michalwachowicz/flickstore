8171e4cfa00f15cf3c97ba9bff37e41ff7b50d5c
michalwachowicz <wachowiczmichal1@gmail.com>
2024-12-23 19:39:16 +0100

    Create movies cache manager

---
+++ src/managers/__tests__/moviesManager.test.ts
@@ -0,0 +1,125 @@
+import { ApiMovie, Movie } from "../../interfaces/Movie";
+import { getMovie, setMovie } from "../moviesManager";
+
+describe("moviesManager", () => {
+  const mockApiMovie: ApiMovie = {
+    id: 1,
+    title: "Test Movie",
+    original_title: "Test Movie Original",
+    overview: "A test movie description",
+    release_date: "2024-01-01",
+    poster_path: "/poster.jpg",
+    backdrop_path: "/backdrop.jpg",
+    runtime: 120,
+    videos: {
+      results: [
+        { id: "vid1", site: "YouTube", name: "Test Trailer" },
+        { id: "vid2", site: "Vimeo", name: "Another Video" },
+      ],
+    },
+    genre_ids: [28, 12],
+    credits: {
+      cast: [
+        {
+          name: "Actor One",
+          known_for_department: "Acting",
+          profile_path: "/actor1.jpg",
+          character: "Hero",
+        },
+      ],
+      crew: [
+        {
+          name: "Director One",
+          known_for_department: "Directing",
+          profile_path: "/director1.jpg",
+          job: "Director",
+        },
+      ],
+    },
+    images: {
+      backdrops: ["/backdrop-2.jpg"],
+    },
+    similar: {
+      results: [
+        {
+          id: 2,
+          title: "Similar Movie",
+          original_title: "Similar Movie Original",
+          overview: "",
+          genre_ids: [28],
+          release_date: "2023-01-01",
+          poster_path: "/poster2.jpg",
+          backdrop_path: "/backdrop2.jpg",
+        },
+      ],
+    },
+  };
+
+  const mockMovie: Movie = {
+    title: "Test Movie Original",
+    releaseDate: "2024-01-01",
+    images: {
+      poster: "/poster.jpg",
+      backdrops: ["/backdrop.jpg", "/backdrop-2.jpg"],
+    },
+    description: "A test movie description",
+    runtime: 120,
+    genres: [28, 12],
+    similar: [2],
+    video: "vid1",
+    credits: {
+      cast: [
+        {
+          name: "Actor One",
+          department: "Acting",
+          image: "/actor1.jpg",
+          character: "Hero",
+        },
+      ],
+      crew: [
+        {
+          name: "Director One",
+          department: "Directing",
+          image: "/director1.jpg",
+          job: "Director",
+        },
+      ],
+    },
+  };
+
+  describe("setMovie()", () => {
+    it("adds Movie object to cache", () => {
+      setMovie(0, mockMovie);
+      expect(getMovie(0)).toBe(mockMovie);
+    });
+
+    it("transforms ApiMovie to Movie object and adds to cache", () => {
+      setMovie(mockApiMovie.id, mockApiMovie);
+      expect(getMovie(mockApiMovie.id)).toEqual(mockMovie);
+    });
+
+    it("adds similar movies to cache recursively", () => {
+      setMovie(mockApiMovie.id, mockApiMovie);
+      expect(getMovie(2)).toEqual({
+        title: "Similar Movie Original",
+        description: "",
+        releaseDate: "2023-01-01",
+        images: {
+          poster: "/poster2.jpg",
+          backdrops: ["/backdrop2.jpg"],
+        },
+        similar: [],
+        genres: [28],
+        credits: {
+          crew: [],
+          cast: [],
+        },
+      });
+    });
+  });
+
+  describe("getMovie()", () => {
+    it("returns undefined if a movie is not cached", () =>
+      expect(getMovie(999)).toBeUndefined());
+  });
+});
---
+++ src/managers/moviesManager.ts
@@ -0,0 +1,63 @@
+import { ApiMovie, Movie } from "../interfaces/Movie";
+import { CastMember, CrewMember } from "../interfaces/Credit";
+import createCreditFromApi from "./creditsManager";
+
+const cache: { [key: number]: Movie } = {};
+
+const getMovie = (id: number) => cache[id];
+
+const isMovie = (movie: unknown): movie is Movie =>
+  typeof movie === "object" && movie !== null && !("overview" in movie);
+
+const createMovieFromApi = (apiMovie: ApiMovie): Movie => {
+  const similar = apiMovie.similar?.results;
+  const similarIds: number[] = [];
+
+  if (similar || Array.isArray(similar)) {
+    similar.forEach((mov) => {
+      const { id } = mov;
+
+      similarIds.push(id);
+      if (!getMovie(id)) cache[id] = createMovieFromApi(mov);
+    });
+  }
+
+  return {
+    title: apiMovie.original_title || apiMovie.title,
+    releaseDate: apiMovie.release_date,
+    images: {
+      poster: apiMovie.poster_path,
+      backdrops: [
+        apiMovie.backdrop_path,
+        ...(apiMovie.images?.backdrops || []),
+      ],
+    },
+    description: apiMovie.overview,
+    runtime: apiMovie.runtime,
+    video: (
+      apiMovie.videos?.results.find(
+        ({ site, name }) =>
+          site === "YouTube" && name.toLowerCase().includes("trailer"),
+      ) || apiMovie.videos?.results[0]
+    )?.id,
+    genres:
+      apiMovie.genre_ids || apiMovie.genres?.map((genre) => genre.id) || [],
+    similar: similarIds,
+    credits: {
+      crew:
+        (apiMovie.credits?.crew?.map((member) =>
+          createCreditFromApi(member),
+        ) as CrewMember[]) || [],
+      cast:
+        (apiMovie.credits?.cast?.map((member) =>
+          createCreditFromApi(member),
+        ) as CastMember[]) || [],
+    },
+  };
+};
+
+const setMovie = (id: number, movie: Movie | ApiMovie) => {
+  cache[id] = isMovie(movie) ? movie : createMovieFromApi(movie);
+};
+
+export { getMovie, setMovie };
