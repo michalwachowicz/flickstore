1075a077228abfbd2d88c02839c1577f8c3e0887
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-04 21:40:48 +0100

    Create shopping cart context and reducer

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react-refresh/only-export-components */
+import React, { createContext, Dispatch, useContext, useReducer } from "react";
+
+interface CartAction {
+  type: "added" | "deleted";
+  id: number;
+}
+
+const CartContext = createContext<number[]>([]);
+const CartDispatchContext = createContext<Dispatch<CartAction>>(() => {});
+
+const cartReducer = (cart: number[], action: CartAction) => {
+  switch (action.type) {
+    case "added":
+      return [...cart, action.id];
+    case "deleted":
+      return cart.filter((id) => id !== action.id);
+    default:
+      throw new Error(`Unknown action: ${action.type}`);
+  }
+};
+
+export const useCart = () => useContext(CartContext);
+export const useCartDispatch = () => useContext(CartDispatchContext);
+
+export const CartProvider = ({ children }: { children: React.ReactNode }) => {
+  const [cart, dispatch] = useReducer(cartReducer, []);
+
+  return (
+    <CartContext.Provider value={cart}>
+      <CartDispatchContext.Provider value={dispatch}>
+        {children}
+      </CartDispatchContext.Provider>
+    </CartContext.Provider>
+  );
+};
