6b8c09945d9f167804dada686a28d5ff2642e556
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-04 14:56:47 +0100

    Create list item button component

---
+++ src/components/ListItemButton.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { Movie } from "../interfaces/Movie";
+import { getImageUrl } from "../api/moviesApi";
+
+interface Props {
+  movie: Movie;
+}
+
+const ListItemButton = ({ movie }: Props) => (
+  <li>
+    <Link
+      to={`/movie/${movie.id}`}
+      role="button"
+      className="flex items-center gap-4"
+    >
+      <img
+        src={getImageUrl(movie.images.poster, 92)}
+        alt={`${movie.title}'s poster`}
+        className="h-auto w-20 rounded"
+        loading="lazy"
+      />
+      <div className="flex w-full items-center justify-between gap-8">
+        <div>
+          <div className="text-xl font-bold text-neutral-200">
+            {movie.title}
+          </div>
+          <div className="mt-2 text-neutral-400">
+            {movie.releaseDate.split("-")[0]}
+          </div>
+        </div>
+        <div className="text-xl font-black text-neutral-200">$19.99</div>
+      </div>
+    </Link>
+  </li>
+);
+
+export default ListItemButton;
---
+++ src/components/__tests__/ListItemButton.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import ListItemButton from "@/Components/ListItemButton";
+
+describe("<ListItemButton />", () => {
+  it("renders properly", () => {
+    const movie = {
+      id: 0,
+      title: "Test",
+      releaseDate: "2025-01-01",
+      images: { poster: "/test" },
+    };
+
+    render(
+      <MemoryRouter>
+        <ListItemButton movie={movie} />
+      </MemoryRouter>,
+    );
+
+    const img = screen.getByRole("img");
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAccessibleName(`${movie.title}'s poster`);
+
+    expect(screen.getByText(movie.title)).toBeInTheDocument();
+    expect(screen.getByText("2025")).toBeInTheDocument();
+  });
+
+  it.todo("routes to the proper movie page");
+});
