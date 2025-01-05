63a699f11572dafe49576c625d4387787dc33768
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-05 16:22:51 +0100

    Wrap app with shopping cart provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { CartProvider } from "./contexts/CartContext";
 import router from "./router/router";
 import "./scss/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>,
 );
