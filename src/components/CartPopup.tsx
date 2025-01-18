f321fa2046133ab5434f41adba4867e48552c63d
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-18 21:51:04 +0100

    Create non-empty state for a cart popup component

--- src/components/CartPopup.tsx
+++ src/components/CartPopup.tsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
+import { Link } from "react-router-dom";
 import DeleteIcon from "@/Assets/images/icons/delete.svg?react";
 import CartIcon from "@/Assets/images/icons/cart.svg?react";
 import IconButton from "@/Components/button/IconButton";
+import ListItemButton from "@/Components/button/ListItemButton";
 import useClosePopup from "../hooks/closePopup";
+import { getMovie } from "../managers/moviesManager";
 
 interface Props {
   cart: number[];
@@ -43,6 +46,28 @@ const CartPopup = ({ cart, onClear = () => {}, onClose = () => {} }: Props) => {
             <p className="mx-4 font-light text-neutral-400">
               Looks like you haven’t added anything to your cart yet
             </p>
+          </div>
+        </div>
+      )}
+      {!isEmpty && (
+        <div className="mt-6">
+          <ul className="flex max-h-56 flex-col gap-4 overflow-y-scroll">
+            {cart.map((item) => (
+              <ListItemButton key={item} movie={getMovie(item)} size="small" />
+            ))}
+          </ul>
+          <div className="mt-6 flex items-center justify-between">
+            <div className="text-xl font-bold text-neutral-50">
+              Total: ${19.99 * cart.length}
+            </div>
+            <Link
+              to="/cart"
+              type="button"
+              className="rounded-lg bg-amber-400 px-6 py-2 text-xl font-bold text-neutral-950 transition-transform ease-in-out hover:scale-105"
+              onClick={() => onClose()}
+            >
+              Checkout
+            </Link>
           </div>
         </div>
       )}
--- src/components/__tests__/CartPopup.test.tsx
+++ src/components/__tests__/CartPopup.test.tsx
@@ -1,7 +1,14 @@
 import { act } from "react";
 import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import CartPopup from "@/Components/CartPopup";
+
+vi.mock("@/Components/button/ListItemButton", () => ({
+  default: ({ movie }: { movie: number }) => (
+    <li data-testid={`list-item-${movie}`} />
+  ),
+}));
 
 describe("<CartPopup />", () => {
   it("renders with empty cart properly", () => {
@@ -20,8 +27,24 @@ describe("<CartPopup />", () => {
     expect(screen.getByText(/0 items/i)).toBeInTheDocument();
   });
 
+  it("renders with items in cart properly", () => {
+    render(
+      <MemoryRouter>
+        <CartPopup cart={[0, 1]} />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getAllByTestId(/list-item/i).length).toEqual(2);
+    expect(screen.getByText(/total: \$39.98/i)).toBeInTheDocument();
+    expect(screen.getByText(/checkout/i)).toBeInTheDocument();
+  });
+
   it("shows the proper amount of items in header", () => {
-    render(<CartPopup cart={[0, 1]} />);
+    render(
+      <MemoryRouter>
+        <CartPopup cart={[0, 1]} />
+      </MemoryRouter>,
+    );
     expect(screen.getByText(/2 items/i)).toBeInTheDocument();
   });
 
@@ -37,7 +60,11 @@ describe("<CartPopup />", () => {
 
   it("clears the cart on clear button click", async () => {
     const fn = vi.fn();
-    render(<CartPopup cart={[0]} onClear={fn} />);
+    render(
+      <MemoryRouter>
+        <CartPopup cart={[0]} onClear={fn} />
+      </MemoryRouter>,
+    );
 
     const user = userEvent.setup();
     await user.click(screen.getByRole("button", { name: /clear the cart/i }));
@@ -77,5 +104,42 @@ describe("<CartPopup />", () => {
       await user.click(screen.getByTestId("outside"));
     });
     expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("closes on checkout button click", async () => {
+    const fn = vi.fn();
+    render(
+      <MemoryRouter>
+        <CartPopup cart={[0, 1]} onClose={fn} />
+      </MemoryRouter>,
+    );
+
+    expect(fn).toHaveBeenCalledTimes(0);
+    const user = userEvent.setup();
+
+    await act(async () => {
+      await user.click(screen.getByText(/checkout/i));
+    });
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it("navigates to cart page on checkout button click", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/cart" element={<div data-testid="cart-page" />} />
+          <Route path="/" element={<CartPopup cart={[0, 1]} />} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    expect(screen.queryByTestId("cart-page")).not.toBeInTheDocument();
+    const user = userEvent.setup();
+
+    await act(async () => {
+      await user.click(screen.getByText(/checkout/i));
+    });
+
+    expect(screen.getByTestId("cart-page")).toBeInTheDocument();
   });
 });
