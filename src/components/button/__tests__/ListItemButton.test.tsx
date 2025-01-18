8318510f1e67f13f6b9cd40021a68ae476b3e2a4
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-19 00:05:59 +0100

    Move the movie page mock to mocks directory

--- src/components/button/__tests__/ListItemButton.test.tsx
+++ src/components/button/__tests__/ListItemButton.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import ListItemButton from "@/Components/button/ListItemButton";
-import MockMoviePage from "./MockMoviePage";
+import MockMoviePage from "@/Components/mocks/MockMoviePage";
 
 describe("<ListItemButton />", () => {
   const movie = {
--- src/components/button/__tests__/MovieInfoButton.test.tsx
+++ src/components/button/__tests__/MovieInfoButton.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import MovieInfoButton from "@/Components/button/MovieInfoButton";
-import MockMoviePage from "./MockMoviePage";
+import MockMoviePage from "@/Components/mocks/MockMoviePage";
 
 describe("<MovieInfoButton />", () => {
   it("renders correctly", () => {
