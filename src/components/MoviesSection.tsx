09927bc5ae5aa8be383188a1a2e4030a753956ad
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-20 22:06:25 +0100

    Add ref to the movies section component

--- src/components/MoviesSection.tsx
+++ src/components/MoviesSection.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* eslint-disable react/require-default-props */
+import React, { ForwardedRef } from "react";
 import { Link } from "react-router-dom";
 import Carousel from "@/Components/carousel/Carousel";
 import ChevronIcon from "@/Assets/images/icons/chevron.svg?react";
@@ -11,28 +12,35 @@ interface Props {
   children: React.ReactNode | React.ReactNode[];
 }
 
-const MoviesSection = ({
-  title,
-  genreId = -1,
-  visibleCount = { sm: 2, md: 4, lg: 6 },
-  children,
-}: Props) => (
-  <section>
-    <div className="mx-6 mb-6 flex items-center gap-4">
-      <h2 className="text-2xl font-bold text-amber-400">{title}</h2>
-      {genreId !== -1 && (
-        <Link
-          to={`/genre/${genreId}`}
-          className="flex items-center gap-1 text-neutral-500"
-        >
-          See more <ChevronIcon className="h-4 w-4 [&>path]:fill-neutral-500" />
-        </Link>
-      )}
-    </div>
-    <Carousel visibleCount={visibleCount} marginX={1.5}>
-      {children}
-    </Carousel>
-  </section>
+const MoviesSection = React.forwardRef(
+  (props: Props, ref: ForwardedRef<HTMLDivElement>) => {
+    const {
+      title,
+      genreId = -1,
+      visibleCount = { sm: 2, md: 4, lg: 6 },
+      children,
+    } = props;
+
+    return (
+      <section ref={ref}>
+        <div className="mx-6 mb-6 flex items-center gap-4">
+          <h2 className="text-2xl font-bold text-amber-400">{title}</h2>
+          {genreId !== -1 && (
+            <Link
+              to={`/genre/${genreId}`}
+              className="flex items-center gap-1 text-neutral-500"
+            >
+              See more{" "}
+              <ChevronIcon className="h-4 w-4 [&>path]:fill-neutral-500" />
+            </Link>
+          )}
+        </div>
+        <Carousel visibleCount={visibleCount} marginX={1.5}>
+          {children}
+        </Carousel>
+      </section>
+    );
+  },
 );
 
 export default MoviesSection;
