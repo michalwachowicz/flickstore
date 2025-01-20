31f46f29c2faf023ec73fc8aa2b5119eeeb3b70e
michalwachowicz <wachowiczmichal1@gmail.com>
2025-01-20 22:45:22 +0100

    Add dynamic genre section loading for home page

--- src/router/routes/HomePage.tsx
+++ src/router/routes/HomePage.tsx
@@ -1,14 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import {
   getCachedPopularMovies,
   getCachedTopRatedMovies,
 } from "../../managers/moviesManager";
 import Hero from "@/Components/hero/Hero";
 import MoviesSection from "@/Components/MoviesSection";
+import {
+  getGenre,
+  getGenreList,
+  getGenreResults,
+} from "../../managers/genresManager";
 
 const HomePage = () => {
   const [popularMovies, setPopularMovies] = useState<number[]>([]);
   const [topRatedMovies, setTopRatedMovies] = useState<number[]>([]);
+  const [sections, setSections] = useState<
+    { id: number; title: string; movies: number[] }[]
+  >([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const lastSectionRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -17,8 +28,55 @@ const HomePage = () => {
 
       const cachedTopRatedMovies = await getCachedTopRatedMovies();
       setTopRatedMovies(cachedTopRatedMovies);
+
+      const firstGenreId = getGenreList()[0];
+      const firstGenre = await getGenreResults(firstGenreId);
+
+      setSections([
+        {
+          id: firstGenreId,
+          title: getGenre(firstGenreId),
+          movies: firstGenre.map((movie) => movie.id),
+        },
+      ]);
     })();
   }, []);
+
+  useEffect(() => {
+    const loadedCount = sections.length;
+    const totalCount = getGenreList().length;
+
+    if (loadedCount === totalCount || !lastSectionRef.current) return () => {};
+    const lastSection = lastSectionRef.current;
+
+    const observer = new IntersectionObserver(
+      async ([entry]) => {
+        if (entry.isIntersecting && !loading) {
+          setLoading(true);
+
+          const nextGenreId = getGenreList()[loadedCount];
+          const results = await getGenreResults(nextGenreId);
+
+          setSections((prev) => [
+            ...prev,
+            {
+              id: nextGenreId,
+              title: getGenre(nextGenreId),
+              movies: results.map((movie) => movie.id),
+            },
+          ]);
+          setLoading(false);
+        }
+      },
+      { threshold: 1.0 },
+    );
+
+    observer.observe(lastSection);
+
+    return () => {
+      observer.unobserve(lastSection);
+    };
+  }, [sections, loading]);
 
   return (
     <div>
@@ -30,6 +88,15 @@ const HomePage = () => {
           movies={topRatedMovies}
           imageType="backdrop"
         />
+        {sections.map(({ id, title, movies }, index) => (
+          <MoviesSection
+            ref={index === sections.length - 1 ? lastSectionRef : null}
+            key={id}
+            genreId={id}
+            title={title}
+            movies={movies}
+          />
+        ))}
       </div>
     </div>
   );
