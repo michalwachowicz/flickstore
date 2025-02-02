60c6e40f77b1f5b0ecf4b25875dc1308fc2c17ae
michalwachowicz <wachowiczmichal1@gmail.com>
2025-02-02 15:16:14 +0100

    Add section header component to movies section

--- src/components/MoviesSection.tsx
+++ src/components/MoviesSection.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/require-default-props */
 import React, { ForwardedRef } from "react";
-import { Link } from "react-router-dom";
 import Carousel from "@/Components/carousel/Carousel";
 import MovieCard from "@/Components/MovieCard";
-import ChevronIcon from "@/Assets/images/icons/chevron.svg?react";
+import SectionHeader from "@/Components/SectionHeader";
 import VisibleCountObject from "../interfaces/VisibleCountObject";
 
 interface Props {
@@ -25,21 +24,11 @@ const MoviesSection = React.forwardRef(
     } = props;
 
     if (movies.length === 0) return null;
+    const route = genreId !== -1 ? `/genre/${genreId}/1` : undefined;
 
     return (
       <section ref={ref}>
-        <div className="mx-6 mb-6 flex items-center gap-4">
-          <h2 className="text-2xl font-bold text-amber-400">{title}</h2>
-          {genreId !== -1 && (
-            <Link
-              to={`/genre/${genreId}/1`}
-              className="flex items-center gap-1 text-neutral-500"
-            >
-              See more{" "}
-              <ChevronIcon className="h-4 w-4 [&>path]:fill-neutral-500" />
-            </Link>
-          )}
-        </div>
+        <SectionHeader title={title} route={route} />
         <Carousel visibleCount={visibleCount} marginX={1.5}>
           {movies.map((movieId) => (
             <MovieCard key={movieId} movieId={movieId} image={imageType} />
